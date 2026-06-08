90b46c427eeeb25df96da6f485afdc5cbb26a59e
Arsh <asing026@uottawa.ca>
2026-06-07 23:38:40 -0400

    Assignment 2 service website added

---
+++ quickfix-site/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h2>QuickFix Cycle Repairs</h2>
+      <p>Fast and affordable bike repair services.</p>
+    </div>
+  );
+}
+
+export default App;
